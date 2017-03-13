52177a57282dbdbe31c97b97dcc0a368871194d6
Khotko <KhotkoGit@gmail.com>
2017-03-13 07:49:43 +0300

    commit Task1

---
+++ MergeSort/MergeSort.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSortNS
+{
+    /// <summary>
+    /// Class that implements Merge sort
+    /// </summary>
+    public class MergeSort
+    {
+        /// <summary>
+        /// Construct a sorted array A by merging two sorted arrays L and R
+        /// </summary>
+        /// <param name="A">Array to be need to sort</param>
+        /// <param name="L">Left part of array A</param>
+        /// <param name="R">Right part of array A</param>
+        private static void Merge(int[] A, int[] L, int[] R)
+        {
+            int i = 0; // i - index of A
+            int j = 0; // j - index of L
+            int k = 0; // k - index of R
+
+            while (j < L.Length && k < R.Length)
+            {
+                if (L[j] <= R[k])
+                {
+                    A[i++] = L[j++];
+                }
+                else
+                {
+                    A[i++] = R[k++];
+                }
+            }
+
+            while (j < L.Length)
+                A[i++] = L[j++];
+
+            while (k < R.Length)
+                A[i++] = R[k++];
+        }
+
+        /// <summary>
+        /// Sort an array using merge algorithm
+        /// </summary>
+        /// <param name="array"></param>
+        public static void Sort(int[] array)
+        {
+            int n = array.Length;
+
+            if (n < 2)
+                return;
+
+            int middle = n / 2;
+
+            // Divide our array into 2 parts. 
+            int[] L = CopyArray(array, 0, middle - 1);
+            int[] R = CopyArray(array, middle, n - 1);
+
+            // Sort Left and Right parts of our array using a recursion
+            Sort(L);
+            Sort(R);
+
+            // Merge sorted left and right parts
+            Merge(array, L, R);            
+        }
+
+        /// <summary>
+        /// Copy an array source from startIndex to endIndex
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="endIndex"></param>
+        /// <returns>Destination array</returns>
+        private static int[] CopyArray(int[] source, int startIndex, int endIndex)
+        {
+            int[] result = new int[endIndex - startIndex + 1];
+
+            int j = 0;
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                result[j++] = source[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MergeSort_ConsoleTesting/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MergeSortNS;
+
+namespace MergeSort_ConsoleTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int[]> TestArrays = new List<int[]>();
+
+            int[] array_1element = new int[1] { 5 };
+            int[] array_2elements = new int[2] { 5, 2 };
+            int[] array_3elements = new int[3] { 1, 5, 3 };
+            int[] array_8elements_NotSorted = new int[8] { 2, 4, 1, 6, 5, 7, 8, 3 };
+            int[] array_8elements_ReverseSorted = new int[8] { 8, 7, 6, 5, 4, 3, 2, 1 };
+            int[] array_8elements_Sorted = new int[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int[] array_8eelements_HasNegativeNumbers = new int[8] { 5, -1000, 1000, -1, 0, 1, -2, -100 };
+            //int[] array_1000elements = GenerateArray(1000);
+            //int[] array_1millionElements = GenerateArray(1000000);
+
+            TestArrays.Add(array_1element);
+            TestArrays.Add(array_2elements);
+            TestArrays.Add(array_3elements);
+            TestArrays.Add(array_8elements_NotSorted);
+            TestArrays.Add(array_8elements_ReverseSorted);
+            TestArrays.Add(array_8elements_Sorted);
+            TestArrays.Add(array_8eelements_HasNegativeNumbers);
+            //TestArrays.Add(array_1000elements);
+            //TestArrays.Add(array_1millionElements);
+
+            PrintArrays(TestArrays, "Unsorted arrays: ");
+
+            foreach (int[] array in TestArrays)
+            {
+                MergeSort.Sort(array);
+            }
+
+            PrintArrays(TestArrays, "Sorted arrays: ");
+
+            Console.ReadLine();
+        }
+
+        private static int[] GenerateArray(int n)
+        {
+            Random random = new Random();
+
+            int[] result = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = random.Next() - random.Next();
+            }
+
+            return result;
+        }
+
+        private static void PrintArrays(IEnumerable<int[]> arrays, string message = "Printing arrays: ")
+        {
+            Console.WriteLine(message);
+
+            foreach (int[] array in arrays)
+            {
+                foreach (int number in array)
+                {
+                    Console.Write("{0} ", number);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
